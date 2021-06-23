8c6f8b790c83f656a411e24c0e9b9c60e82f5d6c
Jacob <jmjohnson1578@gmail.com>
2021-06-23 16:10:24 -0300

    VERSION 4

--- game/constants.py
+++ game/constants.py
@@ -1,5 +1,11 @@
+import os
+
 SCREEN_WIDTH = 700
 SCREEN_HEIGHT = 800
 SCREEN_TITLE = "Battleship!"
 TOTAL_SHIPS = 5
 
+
+file_path = os.getcwd()
+
+BATTLESHIP_IMAGE = file_path + "/battleship-final-project" + "/game/assets/images/battleship.png"
--- game/director.py
+++ game/director.py
@@ -1,12 +1,55 @@
 import arcade
 from game.game_board import Game_Board
+from game.point import Point
 
-class Director:
-    def __init__(self):
+from game import constants
+
+import os #for now
+print(os.getcwd())
+file_path = os.getcwd()
+
+
+
+class Director(arcade.Window):
+    def __init__(input_service,self,width,height,title):
+        super().__init__(width,height,title)
         self.user_board = Game_Board()
         self.computer_board = Game_Board()
+        self.beginning_selector_point = Point(50,50)
+  
+        self.user_ships = arcade.SpriteList()
+        self.computer_ships = arcade.SpriteList()
+        self.empty_spots = arcade.SpriteList()
+        self.all_sprites = arcade.SpriteList()
 
-        print(self.user_board.board)
-        print()
-        print(self.computer_board.board)
+        self.divider_sprite = arcade.Sprite()
+
+        middle_x = constants.SCREEN_WIDTH / 2
+
+        #arcade.draw_rectangle_filled(,center_x=middle_x,center_y=0, width = 10, height=constants.SCREEN_HEIGHT, color=arcade.color.BLUE)
+
+        self.user_spot_coords = []
+        for x in range(50,350,50):
+            for y in range(0,400,50):
+                self.user_spot_coords.append([x,y])
         
+        for item in self.user_spot_coords:
+            x = item[0]
+            y = item[1]
+
+            new_spot_point = Point(x,y)
+
+            new_spot = arcade.Sprite(constants.BATTLESHIP_IMAGE,center_x = x,center_y = y, )
+            self.empty_spots.append(new_spot)
+            new_spot.draw()
+
+    def draw_empty_space(x,y,color):
+        pass
+
+
+
+
+
+    def draw_window(self):
+        pass
+
