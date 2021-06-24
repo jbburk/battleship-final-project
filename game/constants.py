1441f2df2d6db0833bd1f2b6416d4dd796355278
Jacob <jmjohnson1578@gmail.com>
2021-06-23 21:57:07 -0300

    VERSION 5 - Added Board and Dividor

--- game/constants.py
+++ game/constants.py
@@ -9,5 +9,4 @@ TOTAL_SHIPS = 5
 file_path = os.getcwd()
 
 BATTLESHIP_IMAGE = file_path +  "/game/assets/images/battleship_20h.png"
-
-# "/battleship-final-project" +
+EMPTY_SPOT_IMAGE = file_path + "/game/assets/images/placeholder80x20.png"
--- game/input_service.py
+++ game/input_service.py
@@ -1,33 +1,38 @@
 import arcade
 from game.point import Point
-from asciimatics.event import KeyboardEvent
+
 class Input_Service:
     def __init__(self,screen):
         self.screen = screen
-        self.keys = {}
+        self._keys = {}
         self.keys[119] = Point(0, -1) #Up
         self.keys[115] = Point(0, 1) #Left
         self.keys[97] = Point(-1, 0) #Down
         self.keys[100] = Point(1, 0) #Right
         self.keys[13] = "Enter"
 
-    def return_key_press(self):
+
+    def set_key(self, key, modifiers):
+        #Ignoring modifies ar this point...
+        self._keys.append(key)
+
+    def remove_key(self, key, modifiers):
+        self._keys.remove(key)
+
+
+    """def return_key_press(self):
         event = self.screen.get_event()
         if isinstance(event, KeyboardEvent):
             self.current = self.keys.get(event.key_code, self.current)
-            return self.current
+            return self.current"""
 
-    
-"""
-        ***Replaces Above Code***
     def on_key_press(self, key, modifiers):
         # Called whenever a key is pressed.
         if key == arcade.key.up or key == arcade.key.W:
-            Point(0, -1)
+            return Point(0, -1)
         elif key == arcade.key.LEFT or key == arcade.key.A:
-            Point(0, 1)
+            return Point(0, 1)
         elif key == arcade.key.RIGHT or key == arcade.key.D:
-            Point(1, 0)
+            return Point(1, 0)
         elif key == arcade.key.DOWN or key == arcade.key.S:
-            Point(-1, 0)
-"""
+            return Point(-1, 0)
