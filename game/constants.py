0f8b38d7635524560a146e7ef581c6576e974bb1
Jacob <jmjohnson1578@gmail.com>
2021-06-28 13:24:44 -0300

    V8 - Moved most of director to main

--- game/constants.py
+++ game/constants.py
@@ -6,9 +6,11 @@ SCREEN_TITLE = "Battleship!"
 TOTAL_SHIPS = 5
 
 
-file_path = os.getcwd()
+#file_path = os.getcwd()
+file_path = os.path.dirname(os.path.abspath(__file__))
 
-BATTLESHIP_IMAGE = file_path +  "/battleship-final-project/game/assets/images/battleship_20h.png"
-EMPTY_SPOT_IMAGE = file_path + "/battleship-final-project/game/assets/images/placeholder80x20.png"
-DIVIDOR_IMAGE = file_path + "/battleship-final-project/game/assets/images/dividor.png"
-BACKGROUND_IMAGE = file_path + "/battleship-final-project/game/assets/images/water.gif"
+BATTLESHIP_IMAGE = file_path +  "/assets/images/battleship_20h.png"
+EMPTY_SPOT_IMAGE = file_path + "/assets/images/placeholder80x20.png"
+SELECTOR_IMAGE = file_path + "/assets/images/selector.png"
+DIVIDOR_IMAGE = file_path + "/assets/images/dividor.png"
+BACKGROUND_IMAGE = file_path + "/assets/images/water.gif"
